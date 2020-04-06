ca8cf5204d5c6dda0512426144429c311c16158c
unknown <jacob.e.england@gmail.com>
2020-04-06 13:49:57 -0500

    Added get endpoint for user list

---
+++ index.js
@@ -0,0 +1,47 @@
+// dependencies 
+const express = require('express');
+const shortid = require('shortid');
+
+// server 
+const server = express();
+
+// middleware
+server.use(express.json());
+
+// data
+let users = [
+    {
+        id: shortid.generate(),
+        name: "John Computer",
+        bio: "Webdev, coffee junkie, father of 2",
+    },
+    {
+        id: shortid.generate(),
+        name: "Jane Computer",
+        bio: "Eat sleep breathe JavaScript",
+    },
+    {
+        id: shortid.generate(),
+        name: "Tom Computer",
+        bio: "You may know me from my invention, Computer 2",
+    },
+    {
+        id: shortid.generate(),
+        name: "Dustin Computer",
+        bio: "I love heckin doggos and Harry Potter",
+    },
+    {
+        id: shortid.generate(),
+        name: "Britt Computer",
+        bio: "Not from Lisbon",
+    },
+]
+
+// endpoints
+server.get('/api/users', (req, res) => {
+    res.json(users);
+})
+
+// server start code
+const port = 5000;
+server.listen(port, () => console.log(`\n=== api running on port ${port} ===\n`));
