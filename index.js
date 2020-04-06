c3d73ba162cf49178efea3a0ecf03562b1b10391
unknown <jacob.e.england@gmail.com>
2020-04-06 14:04:17 -0500

    Added post endpoint for user list

--- index.js
+++ index.js
@@ -42,6 +42,22 @@ server.get('/api/users', (req, res) => {
     res.json(users);
 })
 
+server.post('/api/users', (req, res) => {
+    newUser = req.body;
+
+    if (newUser.name && newUser.bio) {
+        try {
+            newUser.id = shortid.generate();
+            users.push(newUser);
+            res.status(201).json(users);
+        } catch (error) {
+            res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+        }
+    } else {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    }
+})
+
 // server start code
 const port = 5000;
 server.listen(port, () => console.log(`\n=== api running on port ${port} ===\n`));
